c1d970fe9e89e77d2b3c5969e040d44670d1fbd1
Daiana Natali Sepúlveda <daianas@users.noreply.github.com>
2021-03-15 20:16:55 -0300

    Creación de repositorio en GitHub

---
+++ A886852.Actividad01/A886852.Actividad01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A886852.Actividad01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!" + Environment.NewLine + "Presione una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
